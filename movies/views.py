cfe886d40844c706b47ad94b2f7b9595523b118b
Vinay Joshi <vinay01joshi@gmail.com>
2019-09-08 23:10:50 +0530

    URL parametrs

--- movies/views.py
+++ movies/views.py
@@ -8,3 +8,7 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
